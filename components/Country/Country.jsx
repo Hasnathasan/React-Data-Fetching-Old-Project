1fc1ddc04e32b42abbe0d1717d2ff85f36965113
Hasnathasan <hasnatoooooooo@gmail.com>
2023-12-17 17:17:44 +0600

    code formated

--- components/Country/Country.jsx
+++ components/Country/Country.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import "./Country.css"
+import React from "react";
+import "./Country.css";
 const Country = (props) => {
-    const {name, area, population, flags} = props.country;
-    return (
-        <div className="count">
-            <img src={flags.png} alt="" />
-            <h2>Namuya : {name.common}</h2>
-            <h4>Area : {area}</h4>
-            <h4>Population : {population}</h4>
-        </div>
-    );
+  const { name, area, population, flags } = props.country;
+  return (
+    <div className="count">
+      <img src={flags.png} alt="" />
+      <h2>Namuya : {name.common}</h2>
+      <h4>Area : {area}</h4>
+      <h4>Population : {population}</h4>
+    </div>
+  );
 };
 
 export default Country;
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Countries from '../components/countries/Countries'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Countries from "../components/countries/Countries";
 
 function App() {
   return (
     <div className="app">
       <Countries className="cont"></Countries>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
